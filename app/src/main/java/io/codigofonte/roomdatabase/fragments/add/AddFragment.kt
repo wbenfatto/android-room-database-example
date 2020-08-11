e68aab1210a9407993559adbf84ee5676cef63d4
William Benfatto <wbenfatto@icloud.com>
2020-08-11 01:10:13 -0400

    Consertado layout e ação para o formulario de adicionar dados

--- app/src/main/java/io/codigofonte/roomdatabase/fragments/add/AddFragment.kt
+++ app/src/main/java/io/codigofonte/roomdatabase/fragments/add/AddFragment.kt
@@ -1,20 +1,59 @@
 package io.codigofonte.roomdatabase.fragments.add
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import io.codigofonte.roomdatabase.R
+import io.codigofonte.roomdatabase.data.User
+import io.codigofonte.roomdatabase.data.UserViewModel
+import kotlinx.android.synthetic.main.fragment_add.*
+import kotlinx.android.synthetic.main.fragment_add.view.*
 
 class AddFragment : Fragment() {
+
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view =  inflater.inflate(R.layout.fragment_add, container, false)
+
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        view.add_btn.setOnClickListener {
+            insertDataToDatabase();
+        }
+
+        return view
+    }
+
+    private fun insertDataToDatabase() {
+        val firstName = addFirstName_et.text.toString()
+        val lastName = addLastName_et.text.toString()
+        val age = addAge_et.text
+
+        if(inputCheck(firstName, lastName, age)){
+            val user = User(0, firstName, lastName, Integer.parseInt(age.toString()))
+            mUserViewModel.addUser(user)
+            Toast.makeText(requireContext(), "Successfully added.", Toast.LENGTH_LONG).show()
+
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }else{
+            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean{
+        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
     }
 
 }
